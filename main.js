0c28b658f5ec579489156363489f08a44bdf0a35
brgotcher <38430445+brgotcher@users.noreply.github.com>
2021-08-11 05:55:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var http = require('http');
 import cors from 'cors';
 
 var app = express();
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -10,6 +11,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+*/
 
 const PORT = process.env.PORT || 3412;
 
